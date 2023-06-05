967291a5421459989f8997fdd52e159b7db6ec3a
Rafael Leal <rpsantos@iportsolutions.com.br>
2023-06-05 13:43:00 -0300

    Secao 6

---
+++ Seção 6/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App02_TipoPaginaXF.TipoPagina.Master;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FlyoutPage1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
