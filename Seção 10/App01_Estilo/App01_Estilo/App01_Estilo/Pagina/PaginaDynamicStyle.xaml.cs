d7e10cba32607fd21dcaac319d995beb21265b4a
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-06-04 23:00:39 -0300

    Seção 10

---
+++ Seção 10/App01_Estilo/App01_Estilo/App01_Estilo/Pagina/PaginaDynamicStyle.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_Estilo.Pagina
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaDynamicStyle : ContentPage
+    {
+        public PaginaDynamicStyle()
+        {
+            InitializeComponent();
+        }
+
+        private void MudarCor_Clicado(object sender, EventArgs e)
+        {
+            this.Resources["lblColor"] = Color.Orange;
+
+            this.Resources["lbl"] = this.Resources["newLbl"];
+        }
+    }
+}
