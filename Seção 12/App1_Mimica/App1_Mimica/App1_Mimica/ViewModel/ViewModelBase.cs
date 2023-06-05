1b4b91318751fcab5d11c0d74a7a64244193764a
Rafael Leal <rpsantos@iportsolutions.com.br>
2023-06-05 13:54:19 -0300

    Commit Secao 12

---
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App1_Mimica.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void PropriedadeMudada(string nomePropriedade) 
+        {
+            if (PropertyChanged != null) 
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nomePropriedade));
+            }
+        }
+    }
+}
