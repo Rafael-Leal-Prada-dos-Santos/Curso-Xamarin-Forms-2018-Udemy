bf4f24bbb7b40e5d0eca24ba4562b6ceb670c145
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-06-06 20:43:12 -0300

    Adicionando projeto UWP

---
+++ Seção 12/App1_Mimica/App1_Mimica.UWP/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.Platform.UWP;
+
+namespace App1_Mimica.UWP
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new App1_Mimica.App());
+        }
+    }
+}
