70a3b4aa369a2f8467dfbeb77fcb1421e3421a04
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-06 00:28:52 -0300

    Projeto JWT

---
+++ Seção 13/App3_JWTAsync/App3_JWTAsync/App3_JWTAsync/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App3_JWTAsync.Model;
+using App3_JWTAsync.Service;
+
+namespace App3_JWTAsync
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ObterToken(object sender, EventArgs e)
+        {
+           string resultado = await JWTService.ObterToken(nome.Text, senha.Text);
+
+            lblToken.Text = resultado;
+        }
+
+        private async void VerificarToken(object sender, EventArgs e)
+        {
+            string resultado = await JWTService.Verificar();
+
+            lblRestulado.Text = resultado;
+        }
+    }
+}
---
+++ Seção 13/App3_JWTAsync/App3_JWTAsync/App3_JWTAsync/Service/JWTService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using App3_JWTAsync.Model;
+
+namespace App3_JWTAsync.Service
+{
+    public class JWTService
+    {
+        private static string enderecoBase = "http://ws.spacedu.com.br/xf2018/rest/apix";
+
+        private static string Token;
+        private static string TokenType;
+        public static async Task<string> ObterToken(string nome, string senha) 
+        {
+            string url = enderecoBase + "/token";
+
+            HttpClient clienteHttp = new HttpClient();
+
+            FormUrlEncodedContent conteudoCodificado =
+                new FormUrlEncodedContent(new[] {
+                     new KeyValuePair<string, string>("nome", nome),
+                     new KeyValuePair<string, string>("password", senha),
+                });
+
+            HttpResponseMessage respostaHTTP = 
+                await clienteHttp.PostAsync(url, conteudoCodificado);
+
+            if (respostaHTTP.IsSuccessStatusCode)
+            {
+                RespostaToken token = JsonConvert.DeserializeObject<RespostaToken>(respostaHTTP.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                Token = token.token_type + " " + token.access_token;
+                TokenType = token.token_type;
+                return Token;
+            }
+            else
+            {
+                return respostaHTTP.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+        }
+
+        public static async Task<string> Verificar()
+        {
+            string url = enderecoBase + "/verify";
+
+            HttpClient clienteHttp = new HttpClient();
+
+            clienteHttp.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(TokenType, Token);
+
+            HttpResponseMessage respostaHTTP =
+                await clienteHttp.GetAsync(url);
+
+            if (respostaHTTP.IsSuccessStatusCode)
+            {
+                RespostaVerificar verificar = JsonConvert.DeserializeObject<RespostaVerificar>(respostaHTTP.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                return verificar.usuario.nome + " " + verificar.usuario.id;
+            }
+            else
+            {
+                return respostaHTTP.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
