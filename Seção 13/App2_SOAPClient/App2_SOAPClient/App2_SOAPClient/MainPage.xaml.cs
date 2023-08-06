44e1edae0bb9e94366a9c76d6f8b66875ed968eb
Rafael Santos <rpsantos@iportsolutions.com.br>
2023-08-05 23:41:23 -0300

    Serviço SOAP

---
+++ Seção 13/App2_SOAPClient/App2_SOAPClient/App2_SOAPClient/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2_SOAPClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int numero1 = int.Parse(Num1.Text);
+            int numero2 = int.Parse(Num2.Text);
+
+            txtTexto.Text = DependencyService.Get<IServiceSOAP>().Somar(numero1, numero2);
+        }
+    }
+}
